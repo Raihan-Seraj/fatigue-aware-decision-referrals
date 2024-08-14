350bd1f624e7743237050c68d9176fd155e64c59
Raihan-Seraj <raihan.seraj@mail.mcgill.ca>
2024-08-14 18:40:10 -0400

    added support for multiprocessing and included item for model miss match

--- run_evaluation.py
+++ run_evaluation.py
@@ -7,7 +7,9 @@ import pandas as pd
 from utils import Utils 
 import seaborn as sns
 import matplotlib
+import multiprocessing
 matplotlib.use('Agg')
+
 
 
 '''
@@ -111,13 +113,13 @@ def compute_adp_solution(
     return wl_dp, defrred_idx_dp
 
 
-def compute_performance(betas, simulation_time=100, num_runs=100, result_path='results/'):
+def compute_performance(betas,result_path, simulation_time=100, num_runs=10):
 
     result_dataset = pd.DataFrame(columns=['Algorithm Name','Beta Value','Avg Cost'])
 
-   
-
+    
     for beta in betas:
+
 
         print("Computing peformance with beta = "+str(beta)+'\n')
 
@@ -157,13 +159,18 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
         w_0 = params["w_0"]
 
         ut = Utils(num_tasks_per_batch, mu, lamda, w_0, sigma_a, H0, H1, prior, d_0, beta, sigma_h, ctp, ctn, cfp, cfn, cm, num_bins_fatigue)
+        
+        #initializing the utility with a different value of lambda (different than the one used for training)
+        ut_new = Utils(num_tasks_per_batch, mu, 0.01, w_0, sigma_a, H0, H1, prior, d_0, beta, sigma_h, ctp, ctn, cfp, cfn, cm, num_bins_fatigue)
 
 
         V_bar = np.load(result_path+'num_tasks 20/beta '+str(beta)+'/V_bar.npy')
 
-        all_avg_cost_k = np.zeros(num_runs)
+        all_perf_k = np.zeros(num_runs)
 
-        all_avg_cost_adp = np.zeros(num_runs)
+        all_perf_adp = np.zeros(num_runs)
+
+        all_perf_adp_new = np.zeros(num_runs)
 
         
 
@@ -175,8 +182,11 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
             #initial fatigue for adp
             F_adp = 0
 
+            F_adp_new=0
+
             cost_adp = 0
             cost_k = 0
+            cost_adp_new=0
             for t in range(simulation_time):
 
 
@@ -186,10 +196,14 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
 
                 wl_adp, deferred_idx_adp = compute_adp_solution(batched_posterior_h0,batched_posterior_h1,F_adp, V_bar,ut)
 
+                wl_adp_new, deferred_idx_adp_new = compute_adp_solution(batched_posterior_h0,batched_posterior_h1,F_adp_new, V_bar,ut_new)
+
+                
                 cost_adp += ut.per_step_cost(F_adp,batched_posterior_h1,deferred_idx_adp)
 
                 cost_k += ut.per_step_cost(F_k,batched_posterior_h1,deferred_idx_k)
 
+                cost_adp_new += ut_new.per_step_cost(F_adp_new,batched_posterior_h1,deferred_idx_adp)
 
 
 
@@ -200,19 +214,24 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
 
                 F_adp = ut.get_fatigue(F_adp,wl_adp)
 
-            avg_cost_adp = cost_adp/simulation_time
-            all_avg_cost_adp[run]=avg_cost_adp
+                F_adp_new = ut_new.get_fatigue(F_adp_new,wl_adp_new)
 
-            avg_cost_k = cost_k/simulation_time
-            all_avg_cost_k[run]=avg_cost_k
+            perf_adp = cost_adp
+            all_perf_adp[run]=perf_adp
 
-            new_rows = pd.DataFrame([['ADP', beta, avg_cost_adp],['K-Algorithm',beta,avg_cost_k]], columns=result_dataset.columns)
+            perf_k = cost_k
+            all_perf_k[run]=perf_k
+
+            perf_adp_new = cost_adp_new
+            all_perf_adp_new[run] = perf_adp_new
+
+            new_rows = pd.DataFrame([['ADP', beta, perf_adp],['ADP-corr', beta, perf_adp_new],['K-Algorithm',beta,perf_k]], columns=result_dataset.columns)
             #new_row2 = pd.DataFrame([['K-Algorithm', beta, avg_cost_k]], columns=result_dataset.columns)
 
             result_dataset = pd.concat([result_dataset,new_rows],ignore_index=True)
             
 
-        path1 = 'plot_analysis/'
+        path1 = result_path+'plot_analysis/'
         if not os.path.exists(path1):
             os.makedirs(path1)
         
@@ -226,18 +245,22 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
         if not os.path.exists(path3):
             os.makedirs(path3)
 
-        np.save(path3+'all_avg_cost_adp.npy',all_avg_cost_adp)
+        np.save(path3+'all_perf_adp.npy',all_perf_adp)
 
-        np.save(path3+'all_avg_cost_k.npy',all_avg_cost_k)
+        np.save(path3+'all_perf_adp_new.npy',all_perf_adp_new)
+
+        np.save(path3+'all_perf_k.npy',all_perf_k)
         
-        result_dataset.to_csv('plot_analysis/plot_data.csv')
+        result_dataset.to_csv(result_path+'plot_analysis/plot_data.csv')
 
         sns.boxplot(data=result_dataset, x="Beta Value", y="Avg Cost", hue= "Algorithm Name",gap=0.1)
-        plt.savefig('plot_analysis/cost_comparison/cost_comparison_beta.pdf')
+        plt.savefig(result_path+'plot_analysis/cost_comparison/cost_comparison_beta.pdf')
         plt.clf()
         plt.close()
+
+    return
         
-    return 
+ 
 
 
 
@@ -247,7 +270,7 @@ def compute_performance(betas, simulation_time=100, num_runs=100, result_path='r
 
 
 
-def run_evalutation(beta, simulation_time=100, result_path='results/'):
+def run_evalutation(beta, result_path, simulation_time=100):
 
     ## loading the parameters
 
@@ -290,6 +313,8 @@ def run_evalutation(beta, simulation_time=100, result_path='results/'):
 
     ut = Utils(num_tasks_per_batch, mu, lamda, w_0, sigma_a, H0, H1, prior, d_0, beta, sigma_h, ctp, ctn, cfp, cfn, cm, num_bins_fatigue)
 
+    ut_new = Utils(num_tasks_per_batch, mu, 0.01, w_0, sigma_a, H0, H1, prior, d_0, beta, sigma_h, ctp, ctn, cfp, cfn, cm, num_bins_fatigue)
+
 
     V_bar = np.load(result_path+'num_tasks 20/beta '+str(beta)+'/V_bar.npy')
 
@@ -300,22 +325,30 @@ def run_evalutation(beta, simulation_time=100, result_path='results/'):
     #initial fatigue for adp
     F_adp = 0
 
+    #initial fatigue for the new model
+    F_adp_new=0
+
     fatigue_evolution_kesav = []
     fatigue_evolution_adp = []
+    fatigue_evolution_adp_new = []
 
     taskload_evolution_kesav = []
     taskload_evolution_adp =[]
+    taskload_evolution_adp_new =[]
 
     for t in tqdm(range(simulation_time)):
 
         fatigue_evolution_kesav.append(F_k)
         fatigue_evolution_adp.append(F_adp)
+        fatigue_evolution_adp_new.append(F_adp_new)
 
         batched_obs, batched_posterior_h0, batched_posterior_h1=ut.get_auto_obs()
 
         wl_k, deferred_idx_k = compute_kesavs_algo(batched_posterior_h0, batched_posterior_h1, F_k, ut)
 
         wl_adp, deferred_idx_adp = compute_adp_solution(batched_posterior_h0,batched_posterior_h1,F_adp, V_bar,ut)
+
+        wl_adp_new, deferred_idx_adp_new = compute_adp_solution(batched_posterior_h0,batched_posterior_h1,F_adp_new, V_bar,ut_new)
 
 
 
@@ -327,59 +360,82 @@ def run_evalutation(beta, simulation_time=100, result_path='results/'):
 
         F_adp = ut.get_fatigue(F_adp,wl_adp)
 
+        F_adp_new = ut_new.get_fatigue(F_adp_new,wl_adp_new)
+
         taskload_evolution_adp.append(wl_adp)
+        taskload_evolution_adp_new.append(wl_adp_new)
         taskload_evolution_kesav.append(wl_k)
     
-    return fatigue_evolution_kesav,fatigue_evolution_adp, taskload_evolution_kesav, taskload_evolution_adp
+    return fatigue_evolution_kesav,fatigue_evolution_adp,fatigue_evolution_adp_new, taskload_evolution_kesav, taskload_evolution_adp, taskload_evolution_adp_new
 
 
 
+
+def run_perf_eval(beta, result_path):
+    
+    fatigue_evolution_kesav,fatigue_evolution_adp,fatigue_evolution_adp_new, taskload_evolution_kesav, taskload_evolution_adp, taskload_evolution_adp_new = run_evalutation(beta,result_path)
+
+        ## plotting the level of fatigue 
+
+    path1 = result_path+'plot_analysis/'
+
+    if not os.path.exists(path1):
+        try:
+            os.makedirs(path1,exist_ok=True)
+        except FileExistsError:
+            pass
+    
+    
+
+    plt.plot(fatigue_evolution_adp,label='Approx dynamic program',color='orange')
+    plt.plot(fatigue_evolution_adp_new,label='Approx dynamic program - corr',color='red')
+    plt.plot(fatigue_evolution_kesav,label='Kesav', color='black')
+
+    plt.xlabel('Time')
+    plt.ylabel('Fatigue Level')
+    plt.legend()
+    plt.savefig(path1+'beta_'+str(beta)+'_fatigue.pdf')
+
+    plt.clf()
+    plt.close()
+
+
+    plt.plot(taskload_evolution_adp,label='Approx dynamic program',color='orange')
+    plt.plot(taskload_evolution_adp_new,label='Approx dynamic program-Corr',color='red')
+    plt.plot(taskload_evolution_kesav,label='Kesav', color='black')
+
+    plt.xlabel('Time')
+    plt.ylabel('Workload level')
+    plt.legend()
+    plt.savefig(path1+'beta_'+str(beta)+'_workload.pdf')
+
+    plt.clf()
+    plt.close()
+
+    return
+
+   
 
 
 def main():
 
-    #betas = [0.1,0.3,0.5,0.8]
-    betas = np.round(np.linspace(0.05,0.95,19),2)
+    #betas = np.round(np.linspace(0.1,0.9,9),1)
+    
+    betas = [0.2,0.4,0.6,0.8]
+
+    result_path = "results/"
+
+    inputs = [(beta,result_path) for beta in betas]
     
     
-    for beta in betas:
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        pool.starmap(run_perf_eval, inputs)
 
-        fatigue_evolution_kesav,fatigue_evolution_adp, taskload_evolution_kesav, taskload_evolution_adp = run_evalutation(beta)
+   
+    
+    compute_performance(betas, result_path)
 
-        ## plotting the level of fatigue 
-
-        path1 = 'plot_analysis/'
-
-        if not os.path.exists(path1):
-            os.makedirs(path1)
         
-       
-
-        plt.plot(fatigue_evolution_adp,label='Approx dynamic program',color='orange')
-        plt.plot(fatigue_evolution_kesav,label='Kesav', color='black')
-
-        plt.xlabel('Time')
-        plt.ylabel('Fatigue Level')
-        plt.legend()
-        plt.savefig(path1+'beta_'+str(beta)+'_fatigue.pdf')
-
-        plt.clf()
-        plt.close()
-
-
-        plt.plot(taskload_evolution_adp,label='Approx dynamic program',color='orange')
-        plt.plot(taskload_evolution_kesav,label='Kesav', color='black')
-
-        plt.xlabel('Time')
-        plt.ylabel('Workload level')
-        plt.legend()
-        plt.savefig(path1+'beta_'+str(beta)+'_workload.pdf')
-
-        plt.clf()
-        plt.close()
-    
-    
-    compute_performance(betas)
 
 
 if __name__=='__main__':
