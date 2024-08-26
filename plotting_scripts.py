925f41347ed1bf98f07c3e92cf439f20710ed4ef
Raihan-Seraj <raihan.seraj@mail.mcgill.ca>
2024-08-26 14:51:03 -0400

    added functions to save the refined performance table

--- plotting_scripts.py
+++ plotting_scripts.py
@@ -8,78 +8,150 @@ import seaborn as sns
 
 
 def create_performance_table(betas, result_path):
+
+	total_num_tasks=20
+
+	round_decimal_places=3
       
-      final_data = pd.DataFrame(columns=['Beta','Total Human Cost-ADP', 'Total Human Cost-K','Human Cost Per Taskload-ADP','Human Cost Per Taskload-K',
-                                         'Total Automation Cost-ADP','Total Automation Cost-K', 'Automation Cost Per Taskload-ADP', 'Automation Cost Per Taskload-K','Total Cost-ADP','Total Cost-K'])
-      
-
-      
-      for beta in betas:
-            
-            all_run_human_cost_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_cost_adp.npy'
-
-            all_run_human_cost_k_path = result_path  + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_cost_k.npy'
-
-            all_run_automation_cost_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_auto_cost_adp.npy'
-
-            all_run_automation_cost_k_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_auto_cost_k.npy'
+	final_data = pd.DataFrame(columns=['Beta','Expected Total Human Cost-ADP', ' Expected Total Human Cost-K','Expected Human Cost Per Taskload-ADP','Expected Human Cost Per Taskload-K',
+										'Expected Total Automation Cost-ADP','Expected Total Automation Cost-K', 'Expected Automation Cost Per Taskload-ADP', 'Expected Automation Cost Per Taskload-K','Expected Total Cost-ADP','Expected Total Cost-K'])
 
 
-            all_run_human_cost_adp = np.load(all_run_human_cost_adp_path)
-            all_run_human_cost_k = np.load(all_run_human_cost_k_path)
 
-            all_run_auto_cost_adp = np.load(all_run_automation_cost_adp_path)
-            all_run_auto_cost_k =  np.load(all_run_automation_cost_k_path)
+	for beta in betas:
 
-            # now loading the taskload 
-            all_run_human_tl_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_wl_adp.pkl'
-            
-            all_run_human_tl_k_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_wl_k.pkl'
+		all_run_human_cost_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_cost_adp.npy'
 
-            with open(all_run_human_tl_adp_path,'rb') as file1:
-                  all_human_wl_adp = pickle.load(file1)
+		all_run_human_cost_k_path = result_path  + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_cost_k.npy'
+
+		all_run_automation_cost_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_auto_cost_adp.npy'
+
+		all_run_automation_cost_k_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_auto_cost_k.npy'
 
 
-      
-      
+		all_run_human_cost_adp = np.load(all_run_human_cost_adp_path)
+		all_run_human_cost_k = np.load(all_run_human_cost_k_path)
 
+		all_run_auto_cost_adp = np.load(all_run_automation_cost_adp_path)
+		all_run_auto_cost_k =  np.load(all_run_automation_cost_k_path)
+
+		# now loading the taskload 
+		all_run_human_tl_adp_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_wl_adp.pkl'
+
+		all_run_human_tl_k_path = result_path + 'plot_analysis/cost_comparison/beta '+str(beta)+'/all_human_wl_k.pkl'
+
+		with open(all_run_human_tl_adp_path,'rb') as file1:
+
+			all_run_human_wl_adp = pickle.load(file1)
+
+		with open(all_run_human_tl_k_path, 'rb') as file2:
+			
+			all_run_human_wl_k = pickle.load(file2)
+
+		
+		human_cost_per_wl_per_run_adp = [all_run_human_cost_adp[i]/(sum(all_run_human_wl_adp['Run-'+str(i+1)])) for i in range(len(all_run_human_cost_adp))]
+
+		human_cost_per_wl_per_run_k = [all_run_human_cost_k[i]/(sum(all_run_human_wl_k['Run-'+str(i+1)])) for i in range(len(all_run_human_cost_k))]
+
+		automation_cost_per_wl_per_run_adp = [all_run_auto_cost_adp[i]/(sum(total_num_tasks-all_run_human_wl_adp['Run-'+str(i+1)])) for i in range(len(all_run_auto_cost_adp))]
+
+		automation_cost_per_wl_per_run_k = [all_run_auto_cost_k[i]/(sum(total_num_tasks-all_run_human_wl_k['Run-'+str(i+1)])) for i in range(len(all_run_auto_cost_k))]
+	
+		
+
+		expected_total_human_cost_adp = np.round(np.mean(all_run_human_cost_adp),round_decimal_places)
+		std_total_human_cost_adp = np.round(np.std(all_run_human_cost_adp),round_decimal_places)
+
+		expected_total_human_cost_k = np.round(np.mean(all_run_human_cost_k),round_decimal_places)
+		std_total_human_cost_k = np.round(np.std(all_run_human_cost_k),round_decimal_places)
+
+		expected_human_cost_per_wl_adp = np.round(np.mean(human_cost_per_wl_per_run_adp),round_decimal_places)
+		std_human_cost_per_wl_adp = np.round(np.std(human_cost_per_wl_per_run_adp),round_decimal_places)
+
+		expected_human_cost_per_wl_k = np.round(np.mean(human_cost_per_wl_per_run_k),round_decimal_places)
+		std_human_cost_per_wl_k = np.round(np.std(human_cost_per_wl_per_run_k),round_decimal_places)
+
+		##
+
+		expected_total_automation_cost_adp = np.round(np.mean(all_run_auto_cost_adp),round_decimal_places)
+		std_total_automation_cost_adp = np.round(np.std(all_run_auto_cost_adp),round_decimal_places)
+
+		expected_total_automation_cost_k = np.round(np.mean(all_run_auto_cost_k),round_decimal_places)
+		std_total_automation_cost_k = np.round(np.std(all_run_auto_cost_k),round_decimal_places)
+
+		expected_automation_cost_per_wl_adp = np.round(np.mean(automation_cost_per_wl_per_run_adp),round_decimal_places)
+		std_automation_cost_per_wl_adp = np.round(np.std(automation_cost_per_wl_per_run_adp))
+
+		expected_automation_cost_per_wl_k = np.round(np.mean(automation_cost_per_wl_per_run_k),round_decimal_places)
+		std_automation_cost_per_wl_k = np.round(np.std(automation_cost_per_wl_per_run_k),round_decimal_places)
+
+		expected_total_cost_adp = np.round(np.mean(all_run_human_cost_adp + all_run_auto_cost_adp),round_decimal_places)
+		std_total_cost_adp = np.round(np.std(all_run_human_cost_adp + all_run_auto_cost_adp),round_decimal_places)
+	
+		expected_total_cost_k = np.round(np.mean(all_run_human_cost_k + all_run_auto_cost_k),round_decimal_places)
+		std_total_cost_k = np.round(np.std(all_run_human_cost_k + all_run_auto_cost_k),round_decimal_places)
+	
+
+
+		
+
+		new_row  = pd.DataFrame([[beta, str(expected_total_human_cost_adp)+'+-'+str(std_total_human_cost_adp),
+						   str(expected_total_human_cost_k)+'+-'+str(std_total_human_cost_k),
+						   str(expected_human_cost_per_wl_adp)+'+-'+str(std_human_cost_per_wl_adp),
+						   str(expected_human_cost_per_wl_k)+'+-'+str(std_human_cost_per_wl_k),
+						   str(expected_total_automation_cost_adp)+'+-'+str(std_total_automation_cost_adp),
+						   str(expected_total_automation_cost_k)+'+-'+str(std_total_automation_cost_k),
+						   str(expected_automation_cost_per_wl_adp)+'+-'+str(std_automation_cost_per_wl_adp),
+						   str(expected_automation_cost_per_wl_k)+'+-'+str(std_automation_cost_per_wl_k),
+						   str(expected_total_cost_adp)+'+-'+str(std_total_cost_adp),
+						   str(expected_total_cost_k)+'+-'+str(std_total_cost_k)]],columns=final_data.columns)
+		
+
+		final_data = pd.concat([final_data,new_row],ignore_index=True)
+
+
+	final_data.to_csv(result_path+'refined_performance_table.csv')
+	
+	return
 
 
 def plot_human_perf_vs_taskload(beta, result_path):
 
-    final_path = result_path +'plot_analysis/cost_comparison/beta '+str(beta)+'/'
+	final_path = result_path +'plot_analysis/cost_comparison/beta '+str(beta)+'/'
 
 
-    all_run_hum_wl_adp_path = final_path +'all_human_wl_adp.pkl'
+	all_run_hum_wl_adp_path = final_path +'all_human_wl_adp.pkl'
 
-    all_run_hum_wl_k_path = final_path  + 'all_human_wl_k.pkl'
+	all_run_hum_wl_k_path = final_path  + 'all_human_wl_k.pkl'
 
-    all_hum_cost_adp_path = final_path +'all_human_cost_adp.npy'
+	all_hum_cost_adp_path = final_path +'all_human_cost_adp.npy'
 
-    all_hum_cost_k_path = final_path+'all_human_cost_k.npy'
+	all_hum_cost_k_path = final_path+'all_human_cost_k.npy'
 
 
 
-    with open(all_run_hum_wl_adp_path,'rb') as file1:
-           all_human_wl_adp = pickle.load(file1)
-        
-    
-    with open(all_run_hum_wl_k_path,'rb') as file2:
-           all_human_wl_k = pickle.load(file2)
+	with open(all_run_hum_wl_adp_path,'rb') as file1:
+			all_human_wl_adp = pickle.load(file1)
+		
 
-    
-    all_human_cost_adp = np.load(all_hum_cost_adp_path)
+	with open(all_run_hum_wl_k_path,'rb') as file2:
+			all_human_wl_k = pickle.load(file2)
 
-    all_human_cost_k = np.load(all_hum_cost_k_path)
 
-    import ipdb;ipdb.set_trace()
+	all_human_cost_adp = np.load(all_hum_cost_adp_path)
 
-    return 
+	all_human_cost_k = np.load(all_hum_cost_k_path)
+
+	import ipdb;ipdb.set_trace()
+
+	return 
 
 
 
 if __name__=='__main__':
       
-    result_path = 'results_v2/'
-    beta = 0.5 #
-    plot_human_perf_vs_taskload(beta, result_path)
+	result_path = 'results_v1/'
+
+	betas = [0.1,0.3,0.5,0.7,0.9]
+
+	create_performance_table(betas, result_path)
