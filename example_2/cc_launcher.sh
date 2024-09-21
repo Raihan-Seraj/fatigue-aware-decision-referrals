548b0c5aeed55817e0ae0ea99280671696b4433e
Raihan-Seraj <raihan.seraj@mail.mcgill.ca>
2024-09-21 16:31:43 -0400

    decreased the number of independent samples to 500 and added appropriate arguments

--- example_2/cc_launcher.sh
+++ example_2/cc_launcher.sh
@@ -1,9 +1,9 @@
 
-betas=(1 2 5 7 9)
+alphas=(1 2 5 7 9)
 mus=(0.003 0.05 0.07 0.1)
 lamdas=(0.007 0.03 0.07 0.1)
 
-for beta in ${betas[@]}
+for alpha in ${alphas[@]}
 do
     for mu in ${mus[@]}
 
@@ -20,12 +20,12 @@ do
             #echo "#SBATCH --gres=gpu:2" >> temprun.sh         # ask for 2 GPU
             # echo "#SBATCH --gres=gpu:1" >> temprun.sh         # ask for 2 GPU
             echo "#SBATCH --mem=64G" >> temprun.sh            # ask for 64 GB RAM
-            echo "#SBATCH --time=48:00:00" >> temprun.sh
+            echo "#SBATCH --time=24:00:00" >> temprun.sh
             echo "source ../../thesis/bin/activate" >>temprun.sh
             echo "wandb login e0273d1f1df1e15bffa4b6bca33edb700bc9d54c" >>temprun.s
             echo "#SBATCH --mail-user=raihanseraj@gmail.com" >> temprun.sh
             echo "#SBATCH --mail-type=AL" >> temprun.sh
-            echo "python approximate_dp.py --beta ${beta} --mu ${mu} --lamda ${lamda} --num_expectation_samples 500" >> temprun.sh
+            echo "python approximate_dp.py --alpha ${alpha} --mu ${mu} --lamda ${lamda} --num_expectation_samples 500" >> temprun.sh
             eval "sbatch temprun.sh"
             rm temprun.sh
 
