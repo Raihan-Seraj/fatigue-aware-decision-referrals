fdf482ed6c93c23d9703e26bb9b93e68e5845273
Raihan-Seraj <raihan.seraj@mail.mcgill.ca>
2024-09-24 18:18:33 -0400

    Added example 3

---
+++ example_3/cc_launcher.sh
@@ -0,0 +1,34 @@
+
+alphas=(1 2 5 7 9)
+gammas=(0.003 0.005 0.05 0.07 )
+
+
+for alpha in ${alphas[@]}
+do
+    for gamma in ${gammas[@]}
+
+    do    
+
+        echo "#!/bin/bash" >> temprun.sh
+        echo "#SBATCH --account=def-adityam" >> temprun.sh
+        echo "#SBATCH --output=\"/scratch/raihan08/slurm-%j.out\"" >> temprun.sh
+        # echo "#SBATCH --job-name=TMBIBTEX" >> temprun.sh
+        echo "#SBATCH --cpus-per-task=4"  >> temprun.sh   # ask for 8 CPUs
+        #echo "#SBATCH --gres=gpu:2" >> temprun.sh         # ask for 2 GPU
+        # echo "#SBATCH --gres=gpu:1" >> temprun.sh         # ask for 2 GPU
+        echo "#SBATCH --mem=32G" >> temprun.sh            # ask for 64 GB RAM
+        echo "#SBATCH --time=30:00:00" >> temprun.sh
+        echo "source ../../thesis/bin/activate" >>temprun.sh
+        echo "#SBATCH --mail-user=raihanseraj@gmail.com" >> temprun.sh
+        echo "#SBATCH --mail-type=AL" >> temprun.sh
+        echo "python approximate_dp.py --alpha ${alpha} --gamma ${gamma} --num_expectation_samples 500" >> temprun.sh
+        eval "sbatch temprun.sh"
+        rm temprun.sh
+
+   
+    done
+done
+
+
+
+  
