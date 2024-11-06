bd7fa3c1d09d2d9ea4eb1997ad89a1a0516270d9
Raihan-Seraj <raihanseraj@gmail.com>
2024-11-06 15:51:20 -0500

    initial commit on the stocastic fatigue

--- src/approximate_dp.py
+++ src/approximate_dp.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from utils import Utils
 from run_evaluation import Evaluations
+from envs.fatigue_model_1 import FatigueMDP
 from tqdm import tqdm
 import json
 import os
@@ -21,19 +22,16 @@ Args:
 '''
 
 
-def approximate_dynamic_program(T, num_expectation_samples,ut,fatigue_model='model_1'):
+def approximate_dynamic_program(T, num_expectation_samples,ut):
 
+    env = FatigueMDP()
+
+    num_fatigue_states = env.num_fatigue_states
     
-    if fatigue_model.lower()=='model_1':
-    # number of possible fatigue states
-        F_states = np.round(np.linspace(0, 1, ut.num_bins_fatigue + 1), 2)
-    else:
-        raise ValueError("Invalid fatigue model")
-
     # initializing the value of V_bar
-    V_bar = {t: np.zeros((ut.num_bins_fatigue + 1)) for t in range(T + 2)}
+    V_bar = {t: np.zeros((num_fatigue_states + 1)) for t in range(T + 2)}
     
-    V_bar_k = {t: np.zeros((ut.num_bins_fatigue + 1)) for t in range(T + 2)}
+    V_bar_k = {t: np.zeros((num_fatigue_states + 1)) for t in range(T + 2)}
 
     
     
@@ -41,7 +39,7 @@ def approximate_dynamic_program(T, num_expectation_samples,ut,fatigue_model='mod
     for t in tqdm(range(T, -1, -1)):
 
         # looping over all fatigue states
-        for F_idx, F_t in enumerate(F_states):
+        for  F_t in range(num_fatigue_states):
 
             sum_y = 0
             sum_y_k=0
@@ -49,48 +47,56 @@ def approximate_dynamic_program(T, num_expectation_samples,ut,fatigue_model='mod
             
             for y in range(num_expectation_samples):
                 
-                min_cost = float('inf')
-                batched_obs, batched_posterior_h0, batched_posterior_h1 = ut.get_auto_obs()
-                w_t_k, deferred_k = ut.compute_kesav_policy(F_t,batched_posterior_h0,batched_posterior_h1)
-                for w_t in range(ut.num_tasks_per_batch + 1):
-
-                    ## computing the expectation
-
-                    F_next = ut.get_fatigue(F_t, w_t)
-
-                    F_next_idx = ut.discretize_fatigue_state(F_next)
-
+                expectation_f = 0
+                for F_next_t in range(num_fatigue_states):
+                
+                    min_cost = float('inf')
+                    batched_obs, batched_posterior_h0, batched_posterior_h1 = ut.get_auto_obs()
+                    w_t_k, deferred_k = ut.compute_kesav_policy(F_t,batched_posterior_h0,batched_posterior_h1)
                     
+                    w_t_k_discretized = ut.discretize_taskload(w_t_k)
 
-                    cstar, deferred_indices, _ = ut.compute_cstar(
-                        F_t,
-                        w_t,
-                        batched_posterior_h0,
-                        batched_posterior_h1,
-                    )
+                    w_min = None
+                    for w_t in range(ut.num_tasks_per_batch + 1):
 
-                    auto_cost, hum_cost, deff_cost = ut.per_step_cost(F_t, batched_posterior_h1,deferred_indices)
-                    
-                    per_step_cost = auto_cost + hum_cost + deff_cost
-                    total_cost = per_step_cost + V_bar[t + 1][F_next_idx]
+                        ## computing the expectation
 
-                    
+                        w_t_discrete  = ut.discretize_taskload(w_t)
 
-                    if total_cost < min_cost:
                         
-                        min_cost = total_cost
 
-                V_t = min_cost
+                        cstar, deferred_indices, _ = ut.compute_cstar(
+                            F_t,
+                            w_t_discrete,
+                            batched_posterior_h0,
+                            batched_posterior_h1,
+                        )
+
+                        auto_cost, hum_cost, deff_cost = ut.per_step_cost(F_t, batched_posterior_h1,deferred_indices)
+                        
+                        per_step_cost = auto_cost + hum_cost + deff_cost
+                        total_cost = per_step_cost + V_bar[t + 1][F_next_t]
+
+                        
+
+                        if total_cost < min_cost:
+                            
+                            min_cost = total_cost
+                            w_min = ut.discretize_taskload(w_t)
+                    
+                    expectation_f+=env.P[w_min][F_t,F_next_t]*min_cost
+          
+                V_t = expectation_f
 
                 auto_cost_k,human_cost_k,deferred_cost_k = ut.per_step_cost(F_t,batched_posterior_h1,deferred_k)
 
                 total_cost_k = auto_cost_k + human_cost_k + deferred_cost_k 
                 
-                F_next_k = ut.get_fatigue(F_t,w_t_k)
+                F_next_k = env.next_state(F_t,w_t_k_discretized)
 
-                F_next_idx_k = ut.discretize_fatigue_state(F_next_k)
+               
                 
-                V_t_k = total_cost_k + V_bar_k[t+1][F_next_idx_k] 
+                V_t_k = total_cost_k + V_bar_k[t+1][F_next_t] 
 
                 sum_y += V_t
                 sum_y_k+=V_t_k
@@ -98,8 +104,8 @@ def approximate_dynamic_program(T, num_expectation_samples,ut,fatigue_model='mod
             expected_value = sum_y / num_expectation_samples
             expected_value_k = sum_y_k/ num_expectation_samples
 
-            V_bar[t][F_idx] = expected_value
-            V_bar_k[t][F_idx]= expected_value_k
+            V_bar[t][F_t] = expected_value
+            V_bar_k[t][F_t]= expected_value_k
 
             
 
@@ -213,18 +219,18 @@ def main():
     
     parser = argparse.ArgumentParser(description="Approximate Dynamic Program parameters.")
 
-    parser.add_argument('--alpha', type=float, default= 1, help='The influence of fatigue on false positive probability')
-    parser.add_argument('--beta',type=float, default=0.05, help='The influence of taskload on false positive probability' )
+    parser.add_argument('--alpha', type=float, default= 0.3, help='The influence of fatigue on false positive probability')
+    parser.add_argument('--beta',type=float, default=0.005, help='The influence of taskload on false positive probability' )
     parser.add_argument('--gamma',type=float, default=0.1, help='The exponent of the true positive probability model' )
 
-    parser.add_argument('--mu', type=float, default=0.05,  help='Decay rate of fatigue.')
-    parser.add_argument('--lamda', type=float, default=0.07,  help='Growth rate of fatigue.')
+    parser.add_argument('--mu', type=float, default=0.07,  help='Decay rate of fatigue.')
+    parser.add_argument('--lamda', type=float, default=0.05,  help='Growth rate of fatigue.')
     parser.add_argument('--num_expectation_samples', type=int, default=500, help='Number of expectation samples to take for the approximate Dynamic Program.')
-    parser.add_argument('--horizon', type=int, default=15, help='The length of the horizon.')
+    parser.add_argument('--horizon', type=int, default=10, help='The length of the horizon.')
     parser.add_argument('--d_0',type=float, default= 3, help='The value of d0 in the experiment.')
     parser.add_argument('--prior',default=[0.8,0.2], nargs=2, type=float, help='A list containing the prior of [H0, H1].' )
     parser.add_argument('--num_tasks_per_batch', type=int, default=20, help='The total number of tasks in a batch.')
-    parser.add_argument('--sigma_a',type=float, default=2.5, help='Automation observation channel variance.')
+    parser.add_argument('--sigma_a',type=float, default=2, help='Automation observation channel variance.')
     parser.add_argument('--w_0', type=int, default=15, help='The workload threshold beyond which fatigue recovery does not occur')
     parser.add_argument('--num_bins_fatigue', type=int, default=10, help='The number of bins to be used to discretize fatigue')
 
@@ -243,7 +249,7 @@ def main():
     parser.add_argument('--results_path', type=str, default='results/', help='The name of the directory to save the results')
 
     parser.add_argument('--run_eval_only', type=bool, default=False)
-    parser.add_argument('--num_eval_runs', type=int, default=10, help="Number of independent runs for monte carlo performance evaluation")
+    parser.add_argument('--num_eval_runs', type=int, default=500, help="Number of independent runs for monte carlo performance evaluation")
 
     args = parser.parse_args()
 
---
+++ src/envs/fatigue_model_1.py
@@ -0,0 +1,61 @@
+import numpy as np
+import copy
+
+
+
+
+class FatigueMDP():
+
+    def __init__(self):
+
+        self.num_fatigue_states = 4
+        self.num_actions = 4
+        self.start_state_probs = np.ones(self.num_fatigue_states)/(self.num_fatigue_states)
+        self.start_state = np.random.choice(len(self.start_state_probs),p=self.start_state_probs)
+
+
+        self.current_state = copy.deepcopy(self.start_state)
+
+        # self.action_transition_matrix = {0: np.array([[0.5, 0.3,0.1,0.1],
+        #                                               [0.4, 0.3, 0.2, 0.1],
+        #                                               [0.3,0.4,0.2,0.1],
+        #                                               0.3,0.3,0.2,0.2]), 
+                                                      
+        #                                  1: np.array([[]])            }
+
+        self.P = {0: np.array([[0.7,0.3,0,0],
+                                [0.5, 0.4, 0.1, 0],
+                                [0.3, 0.4, 0.3, 0],
+                                [0.6, 0.3, 0.1, 0]]),
+                                
+                 1: np.array([[0.4, 0.5, 0.1, 0],
+                                [0.2, 0.4, 0.3, 0.1],
+                                [0.1, 0.2, 0.4, 0.3],
+                                [0,0.1,0.2,0.7]]),
+                                
+                 2: np.array([[0.2,0.3,0.4,0.1],
+                            [0.1,0.2,0.4,0.3],
+                            [0,0.1,0.3,0.6],
+                            [0,0,0.2,0.8]]),
+                 
+                 3: np.array([[0.1, 0.2, 0.4, 0.3],
+                            [0,0.1,0.3,0.6],
+                            [0,0,0.2,0.8],
+                            [0,0,0.1,0.9]])
+                            
+                            }
+
+    def step(self, action):
+
+        next_state = np.random.choice(self.num_fatigue_states, p=self.P[action][self.current_state,:])
+
+        self.current_state = next_state
+
+        return next_state
+    
+    def reset(self):
+
+        self.current_state = self.start_state
+
+        return self.current_state
+
