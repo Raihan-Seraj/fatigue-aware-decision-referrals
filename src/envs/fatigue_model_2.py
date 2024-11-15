8e6a54e8e883954e2e79502ad13237a1dfef4cb6
Raihan-Seraj <raihanseraj@gmail.com>
2024-11-15 13:14:25 -0500

    added a second model for fatigue

---
+++ src/envs/fatigue_model_2.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+
+
+
+class FatigueMDP2():
+
+    def __init__(self):
+
+        self.num_fatigue_states = 11
+        self.num_actions = 21
+        self.start_state = 0
+        self.w_0  = 10
+        self.mu = 0.03
+        self.lamda = 0.07
+        self.num_bins_fatigue = 10
+        self.discrete_bins = np.linspace(0, 1, self.num_bins_fatigue+1)
+        
+
+    def next_state(self, current_state, taskload):
+
+       
+
+        R_t = current_state #* np.exp(-self.mu * max((self.w_0 - taskload), 0))
+
+        F_next = R_t + (1 - R_t) * (1 - np.exp(-self.lamda * taskload))
+
+        # we need to discretize the taskload here 
+        
+       
+        discrete_bins = np.digitize(F_next, self.discrete_bins)
+       
+        next_state = F_next
+        return next_state, discrete_bins
+    
+    
